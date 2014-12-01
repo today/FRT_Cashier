ee5c91b68a2ffa3e8f59a299ee6f25047e901bc8
Jin Tian <today.xa@gmail.com>
2014-12-01 13:59:05 +0800

    很久没有提交了哦。程序结构，页面，读入药材列表，页面动作都已经完成了。

---
+++ js/FRT_cashier.js
@@ -0,0 +1,142 @@
+var fs = require('fs');
+var _ = require('underscore');
+
+
+function isblank(strA){
+	if(strA){
+		if( "string" === typeof(strA) ){
+			if( "" === strA.trim()){
+				return true;
+			}else{
+				return false;
+			}
+		}else{
+			return false;
+		}
+	}else{
+		return true;
+	}
+}
+
+function getYMD( aDate ){
+	var year = aDate.getFullYear();
+	var month = aDate.getMonth()+1;
+	var day = aDate.getDate();
+
+	return "" + year + "-" + month + "-" + day;
+
+}
+
+function getYMD_no_dash( aDate ){
+	var year = aDate.getFullYear();
+	var month = aDate.getMonth()+1;
+	var day = aDate.getDate();
+
+	return "" + year + month + day;
+
+}
+
+
+function makeBlankLine(aDate){
+	var obj = new Object();
+	
+	obj.treat_date = getYMD( aDate );
+	obj.patient_no =  "0";
+    obj.case_no =  "0";
+    obj.mobile =  "";
+    obj.name =  "";
+    obj.comment =  "";
+	return obj;
+}
+
+// 把内容附加到文件中的 Json 数组中，作为最后一个成员。
+function appendToJsonFile( filename, aJsonObj ){
+	
+	// 打开文件，读入json串。  解析为json对象。
+	var fileStr = fs.readFileSync(filename);
+
+	//console.log("getBooking(): " + strBookingList);
+    var jsonObj = JSON.parse(fileStr);
+
+    // 将内容append为最后一个成员
+    jsonObj.push(aJsonObj)
+
+	var jsonStr = JSON.stringify(jsonObj);
+	fs.writeFileSync( filename, jsonStr);
+}
+
+/*  读文件并且转为Json返回   */
+function readFileToJson( filename ){
+	
+	aJson = "{'runResult':'start run.'}"
+	if(fs.existsSync( filename) ){
+		var strFileContent = fs.readFileSync(filename);   
+		//console.log("getBooking(): " + strBookingList);
+	    aJson = JSON.parse(strFileContent);
+	}
+	return aJson;
+}
+
+
+document.onkeydown = function(event) { 
+    keynum = event.which;
+
+    // 这一段是用来显示隐藏debug区的。
+    if (keynum==192){
+      
+      if( debug_flag == 0 ){
+        debug_flag++;
+      }else if(debug_flag == 1 ){
+        debug_flag++;
+        document.getElementById('debug_area').style.display="";//显示
+        
+      }
+      else if(debug_flag == 2 ){
+        debug_flag = 0;
+        document.getElementById('debug_area').style.display="none";//隐藏
+      }
+        
+    }
+
+    //alert(keynum);
+    if (keynum==13){
+        
+        var skip_num = 0;
+        var temp_id = event.srcElement.id;
+        if( temp_id ){
+          var str_id = temp_id.substring(3); 
+          //console.log("id:" , str_id );
+          var i_id = new Number(str_id);
+
+          while ( skip_num < 5 ){  // allow skip num , 
+            
+            var obj = document.getElementById('id_' + (i_id + 1 + skip_num) );
+            if( obj ){
+              obj.focus();
+              obj.select();
+              return;
+            }
+            skip_num++;
+          }
+          
+          // loop to 0
+          obj = document.getElementById( 'id_0' );
+          obj.focus();
+          obj.select();
+          
+        }
+        else{
+          console.log("not input." );
+        }
+        return;
+    }
+  };
+
+
+
+
+
+
+
+
+
